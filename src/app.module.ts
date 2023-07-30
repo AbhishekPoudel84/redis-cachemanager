981d71970cd9734285cf3cb7649c6f28e3d4c3ad
Abhishek Poudel <abhishekpoudel84@gmail.com>
2023-07-30 12:03:21 +0545

    caching

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      // ttl: 60,
+      // max: 1000,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor, //for every controller in the system
+    },
+  ],
 })
 export class AppModule {}
